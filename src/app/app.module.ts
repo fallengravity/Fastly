8cce2e823541ca8fbede6a61717b10218b7d49f0
FallenGravity <ethanvb55@gmail.com>
2019-11-29 15:52:14 +0200

    Tune up Working implementations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
-const config: SocketIoConfig = { url: 'http://localhost:3001', options: {} };
+const config: SocketIoConfig = { url: 'https://socket.xerom.org', options: {} };
 
 @NgModule({
   declarations: [AppComponent],
