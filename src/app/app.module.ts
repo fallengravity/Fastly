d0b29ebc43cf7dd3909a12eb617d3b52d9b23e16
FallenGravity <ethanvb55@gmail.com>
2019-11-29 14:31:34 +0200

    Working Socket.io Chat Implementation.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+const config: SocketIoConfig = { url: 'http://localhost:3001', options: {} };
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, SocketIoModule.forRoot(config)],
   providers: [
     StatusBar,
     SplashScreen,
@@ -20,4 +23,5 @@ import { AppComponent } from './app.component';
   ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
--- src/app/chat/chat.page.ts
+++ src/app/chat/chat.page.ts
@@ -1,48 +1,56 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { IonContent } from "@ionic/angular";
+import { Component, OnInit } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.page.html',
   styleUrls: ['./chat.page.scss'],
 })
-export class ChatPage {
+export class ChatPage implements OnInit {
+  message = '';
+  messages = [];
+  currentUser = '';
+
+  constructor(private socket: Socket, private toastCtrl: ToastController) { }
 
   ngOnInit() {
-    this.content.scrollToBottom();
+    this.socket.connect();
+
+    let name = `user-${new Date().getTime()}`;
+    this.currentUser = name;
+
+    this.socket.emit('set-name', name);
+
+    this.socket.fromEvent('users-changed').subscribe(data => {
+      let user = data['user'];
+      if (data['event'] === 'left') {
+        this.showToast('User left: ' + user);
+      } else {
+        this.showToast('User joined: ' + user);
+      }
+    });
+
+    this.socket.fromEvent('message').subscribe(message => {
+      this.messages.push(message);
+    });
   }
 
-  messages = [
-    {
-      user: 'simon',
-      msg: 'Hey Fallen, Cool App!'
-    },
-    {
-      user: 'fallen',
-      msg: 'Yeah Bro!'
-    },
-    {
-      user: 'simon',
-      msg: 'When will it be released???'
-    }
-  ];
-
-  currentUser = 'fallen';
-  otherUser = 'Dylie';
-  userMSG = '';
-
-  @ViewChild('content', { static: true }) private content: any;
-
-  constructor() { }
-
   sendMessage() {
-    this.messages.push({
-      user: 'fallen',
-      msg: this.userMSG
+    this.socket.emit('send-message', { text: this.message });
+    this.message = '';
+  }
+
+  ionViewWillLeave() {
+    this.socket.disconnect();
+  }
+
+  async showToast(msg) {
+    let toast = await this.toastCtrl.create({
+      message: msg,
+      position: 'top',
+      duration: 2000
     });
-    this.userMSG = '';
-    setTimeout(() => {
-      this.content.scrollToBottom();
-    });
+    toast.present();
   }
 }
